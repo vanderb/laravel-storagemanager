c7171e829b503cc4eead98d51dba393299e63d3a
Freerk van Zeijl <freerk@freerks-imac.intern>
2018-11-12 17:06:09 +0100

    Created ServiceProvider.

---
+++ src/Storagemanager/StoragemanagerServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Vanderb\Storagemanager;
+
+use Illuminate\Support\ServiceProvider;
+
+class StoragemanagerServiceProvider extends ServiceProvider {
+    /**
+     * Boot the service provider.
+     */
+    
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        
+    }
+    
+}
